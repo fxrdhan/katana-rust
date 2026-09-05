d486a50aeaaee8b219bbff2757668b820e7672fd
Firdaus Arif R <firdausarief65@gmail.com>
2026-09-05 22:55:51 +0700

    style(core): format extensions array in accordance with rustfmt

--- crates/katana-core/src/extensions.rs
+++ crates/katana-core/src/extensions.rs
@@ -3,13 +3,12 @@ use url::Url;
 
 /// Default list of static binary, media, and archive extensions to be filtered from crawling.
 pub const DEFAULT_EXT_FILTER: &[&str] = &[
-    ".3g2", ".3gp", ".7z", ".apk", ".arj", ".avi", ".axd", ".bmp", ".csv", ".deb", ".dll",
-    ".doc", ".drv", ".eot", ".exe", ".flv", ".gif", ".gifv", ".gz", ".h264", ".ico", ".iso",
-    ".jar", ".jpeg", ".jpg", ".lock", ".m4a", ".m4v", ".map", ".mkv", ".mov", ".mp3", ".mp4",
-    ".mpeg", ".mpg", ".msi", ".ogg", ".ogm", ".ogv", ".otf", ".pdf", ".pkg", ".png", ".ppt",
-    ".psd", ".rar", ".rm", ".rpm", ".svg", ".swf", ".sys", ".tar.gz", ".tar", ".tif", ".tiff",
-    ".ttf", ".txt", ".vob", ".wav", ".webm", ".webp", ".wmv", ".woff", ".woff2", ".xcf",
-    ".xls", ".xlsx", ".zip",
+    ".3g2", ".3gp", ".7z", ".apk", ".arj", ".avi", ".axd", ".bmp", ".csv", ".deb", ".dll", ".doc",
+    ".drv", ".eot", ".exe", ".flv", ".gif", ".gifv", ".gz", ".h264", ".ico", ".iso", ".jar",
+    ".jpeg", ".jpg", ".lock", ".m4a", ".m4v", ".map", ".mkv", ".mov", ".mp3", ".mp4", ".mpeg",
+    ".mpg", ".msi", ".ogg", ".ogm", ".ogv", ".otf", ".pdf", ".pkg", ".png", ".ppt", ".psd", ".rar",
+    ".rm", ".rpm", ".svg", ".swf", ".sys", ".tar.gz", ".tar", ".tif", ".tiff", ".ttf", ".txt",
+    ".vob", ".wav", ".webm", ".webp", ".wmv", ".woff", ".woff2", ".xcf", ".xls", ".xlsx", ".zip",
 ];
 
 /// Normalizes an extension string to lower-case with a leading dot.
